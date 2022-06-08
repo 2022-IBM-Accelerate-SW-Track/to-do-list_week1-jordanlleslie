dba90f557cce933f3f8d2a60051ce354a7ca387d
jordanlleslie <78113256+jordanlleslie@users.noreply.github.com>
2022-06-08 17:52:43 -0400

    added some details on About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,6 +6,27 @@ export default class About extends Component {
       <div>
         <p>Design your About me page </p> 
       </div>
+      
+      
+      <div>
+        <div class="split left">
+          <div className="centered">
+            <img 
+              className="profile_image"
+              src=""
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Jordan Leslie</div>
+            <div className="brief_description">
+              Hi! My name is Jordan. I'm from Atlanta, GA and I'm a junior at Emory University studying CS and Spanish.
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
